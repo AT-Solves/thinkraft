0385aac136a26fc34adae6eece70b2f84ae30391
achieverthoughts <achieverthoughts@outlook.com>
2026-02-08 00:24:11 +0530

    Fix GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/thinkraft/',   // REQUIRED for subpath deployment
+    base: '/Thinkraft/',   // REQUIRED for subpath deployment
 })
