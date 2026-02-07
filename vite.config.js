6eb023edf8aaac31791edd51af033762ae60d461
achieverthoughts <achieverthoughts@outlook.com>
2026-02-08 01:18:11 +0530

    Routing Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+
     plugins: [react()],
-    base: '/Thinkraft/',   // REQUIRED for subpath deployment
-})
+
+    base: command === 'build'
+        ? '/Thinkraft/'   // GitHub Pages production
+        : '/',            // local development
+
+}))
